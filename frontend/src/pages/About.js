a638215b32efe4e765a125214f95214798f4ffa3
Hayden <hayden.karl@slu.edu>
2024-10-08 13:08:02 -0500

    set up the react project, added basic webpages, added basic navigation links

---
+++ frontend/src/pages/About.js
@@ -0,0 +1,13 @@
+// src/pages/About.js
+import React from 'react';
+import '../styles.css';
+
+function About() {
+  return (
+    <div className="page-container">
+      <h1>ABOUT</h1>
+    </div>
+  );
+}
+
+export default About;
---
+++ frontend/src/pages/Menu.js
@@ -0,0 +1,13 @@
+// src/pages/Menu.js
+import React from 'react';
+import '../styles.css';
+
+function Menu() {
+  return (
+    <div className="page-container">
+      <h1>MENU</h1>
+    </div>
+  );
+}
+
+export default Menu;
