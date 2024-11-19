37344bfa6026ad4ad74b66d0554d71a872903a5f
Sam Lyskawa <samuel.lyskawa@slu.edu>
2024-11-19 16:58:09 -0600

    stashing

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,19 +1,24 @@
-// src/components/Header.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../img/SHHRestaurantLogo.jpg';
 import '../styles.css';
 
 function Header() {
   return (
     <header className="header">
-      <nav>
-        <ul className="nav-list">
-          <li><Link to="/">HOME</Link></li>
-          <li><Link to="/booking">BOOKING</Link></li>
-          <li><Link to="/menu">MENU</Link></li>
-          <li><Link to="/about">ABOUT</Link></li>
-        </ul>
-      </nav>
+      <div className="header-container">
+        <div className="logo-container">
+          <img src={logo} alt="Restaurant Logo" className="restaurant-logo" />
+        </div>
+        <nav className="nav-container">
+          <ul className="nav-list">
+            <li><Link to="/">HOME</Link></li>
+            <li><Link to="/booking">BOOKING</Link></li>
+            <li><Link to="/menu">MENU</Link></li>
+            <li><Link to="/about">ABOUT</Link></li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
